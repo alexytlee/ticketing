e5d175298e0e74fcd1130d4e8ca53f76e20732b2
alexytlee <alexytlee@gmail.com>
2020-10-03 19:03:11 +0800

    finished the signup/signin/signout flow

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponenet = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponenet.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponenet;

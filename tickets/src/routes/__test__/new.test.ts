1fef389010ed5cd39272de94f9055d49305a4a18
alexytlee <alexytlee@gmail.com>
2020-10-05 22:45:51 +0800

    fixed testing and updated env variables

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route hanlder listening to /api/tickets for post requets', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -47,13 +48,26 @@ it('creates a ticket with valid parameters', async () => {
   let tickets = await Ticket.find({});
   expect(tickets.length).toEqual(0);
 
+  const title = 'asdf';
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
-    .send({ title: 'asdf', price: 20 })
+    .send({ title, price: 20 })
     .expect(201);
 
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(20);
+});
+it('publishes an event', async () => {
+  const title = 'asdf';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({ title, price: 20 })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });

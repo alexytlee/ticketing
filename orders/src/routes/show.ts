f9ea663621d80ac616a9ec93333df13aa6b8f8eb
alexytlee <alexytlee@gmail.com>
2020-10-07 22:08:03 +0800

    added more tests for the listeners

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -13,12 +13,14 @@ router.get(
   requireAuth,
   async (req: Request, res: Response) => {
     const order = await Order.findById(req.params.orderId).populate('ticket');
+
     if (!order) {
       throw new NotFoundError();
     }
     if (order.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
+
     res.send(order);
   }
 );
--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/api/tickets/:id', async (req: Request, res: Response) => {
   const ticket = await Ticket.findById(req.params.id);
+
   if (!ticket) {
     throw new NotFoundError();
   }
